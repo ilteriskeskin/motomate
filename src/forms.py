15a81009a3840084c06bc4ddd846ff5f29201aa0
Ali İlteriş Keskin <ilteriskeskin@pop-os.localdomain>
2021-05-08 02:30:27 +0300

    Added new template and create tour func

--- src/forms.py
+++ src/forms.py
@@ -1,18 +1,46 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextAreaField, IntegerField
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextAreaField, IntegerField, DateField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    email = StringField("Email", validators=[validators.Length(min=7, max=50), validators.DataRequired(message="Lütfen Bu Alanı Doldurun")])
-    password = PasswordField("Parola", validators=[validators.DataRequired(message="Lütfen Bu Alanı Doldurun")])
 
-# Kullanıcı kayıt formu
+class TourForm(Form):
+    name = StringField("Full Name", validators=[validators.Length(
+        min=3, max=40), validators.DataRequired(message="Please fill this field")])
+    email = StringField("Email", validators=[validators.Email(
+        message="Enter a real email address"), validators.DataRequired(message="Please fill this field")])
+    twitter_username = StringField("Twitter username")
+    instagram_username = StringField("Instagram username")
+    telegram_username = StringField("Telegram username")
+    from_city = StringField("From City", validators=[
+                            validators.DataRequired(message="Please fill this field")])
+    to_city = StringField("To City", validators=[
+                          validators.DataRequired(message="Please fill this field")])
+    motorcycle_brand = StringField("Motorcycle brand", validators=[
+                                   validators.DataRequired(message="Please fill this field")])
+    engine_capacity = StringField("Engine Capacity (125cc)", validators=[
+                                  validators.DataRequired(message="Please fill this field")])
+    tour_date = StringField("Tour Date", validators=[
+                       validators.DataRequired(message="Please fill this field")])
+    note = TextAreaField("Note", validators=[validators.Length(max=240)])
+
+
+class LoginForm(Form):
+    email = StringField("Email", validators=[validators.Length(
+        min=7, max=50), validators.DataRequired(message="Please fill this field")])
+    password = PasswordField("Password", validators=[
+                             validators.DataRequired(message="Please fill this field")])
+
 
 class RegisterForm(Form):
-    name = StringField("Ad", validators=[validators.Length(min=3, max=25), validators.DataRequired(message="Lütfen Bu Alanı Doldurun")])
-    username = StringField("Kullanıcı Adı", validators=[validators.Length(min=3, max=25), validators.DataRequired(message="Lütfen Bu Alanı Doldurun")])
-    email = StringField("Email", validators=[validators.Email(message="Lütfen Geçerli Bir Email Adresi Girin")])
-    password = PasswordField("Parola", validators=[
-        validators.DataRequired(message="Lütfen Bu Alanı Doldurun"),
-        validators.EqualTo(fieldname="confirm", message="Parolalarınız Uyuşmuyor")
+    name = StringField("Full Name", validators=[validators.Length(
+        min=3, max=25), validators.DataRequired(message="Please fill this field")])
+    username = StringField("Username", validators=[validators.Length(
+        min=3, max=25), validators.DataRequired(message="Please fill this field")])
+    email = StringField("Email", validators=[validators.Email(
+        message="Enter a real email address")])
+    password = PasswordField("Password", validators=[
+        validators.DataRequired(message="Please fill this field"),
+        validators.EqualTo(fieldname="confirm",
+                           message="Your passwords do not match")
     ])
-    confirm = PasswordField("Parola Doğrula", validators=[validators.DataRequired(message="Lütfen Bu Alanı Doldurun")])
+    confirm = PasswordField("Verify password", validators=[
+                            validators.DataRequired(message="Please fill this field")])
--- src/main.py
+++ src/main.py
@@ -1,61 +1,124 @@
 from flask import Flask, render_template, flash, redirect, request, session, logging, url_for
 from flask_sqlalchemy import SQLAlchemy
-from forms import LoginForm, RegisterForm
+from forms import TourForm, LoginForm, RegisterForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'linuxdegilgnulinux'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ilteriskeskin/Belgeler/mototourmate/src/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String(15), unique=True)
+    name = db.Column(db.String(15))
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(25), unique=True)
+    password = db.Column(db.String(25))
+
+
+class TourPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    creator = db.Column(db.String(80))
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    twitter_username = db.Column(db.String(80), unique=True)
+    instagram_username = db.Column(db.String(80), unique=True)
+    telegram_username = db.Column(db.String(80), unique=True)
+    from_city = db.Column(db.String(80))
+    to_city = db.Column(db.String(80))
+    motorcycle_brand = db.Column(db.String(80))
+    engine_capacity = db.Column(db.String(80))
+    tour_date = db.Column(db.String(80))
+    note = db.Column(db.String(80))
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    tours = TourPost.query.all()
+    return render_template('home.html', tours=tours)
 
 
-@app.route('/login/', methods = ['GET', 'POST'])
+@app.route('/create-tour/', methods=['GET', 'POST'])
+@login_required
+def create_tour():
+    form = TourForm(request.form)
+    if request.method == 'POST' and form.validate:
+        new_tour = TourPost(
+            creator=session['email'],
+            name=form.name.data,
+            email=form.email.data,
+            twitter_username=form.twitter_username.data,
+            instagram_username=form.instagram_username.data,
+            telegram_username=form.telegram_username.data,
+            from_city=form.from_city.data.lower(),
+            to_city=form.to_city.data.lower(),
+            motorcycle_brand=form.motorcycle_brand.data,
+            engine_capacity=form.engine_capacity.data.lower().replace(' ', ''),
+            tour_date=form.tour_date.data,
+            note=form.note.data
+        )
+
+        db.session.add(new_tour)
+        db.session.commit()
+        flash('New tour created!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('create-tour.html', form=form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate:
-        user = User.query.filter_by(email = form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
-                flash("Başarıyla Giriş Yaptınız", "success")
-                
+                flash("Login is success!", "success")
+
                 session['logged_in'] = True
-                session['email'] = user.email 
+                session['email'] = user.email
 
                 return redirect(url_for('home'))
             else:
-                flash("Kullanıcı Adı veya Parola Yanlış", "danger")
+                flash("Wrong email or password", "danger")
                 return redirect(url_for('login'))
 
-    return render_template('auth/login.html', form = form)
+    return render_template('auth/login.html', form=form)
 
 
-@app.route('/register/', methods = ['GET', 'POST'])
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(name = form.name.data, username = form.username.data, email = form.email.data, password = hashed_password)
+        hashed_password = generate_password_hash(
+            form.password.data, method='sha256')
+        new_user = User(name=form.name.data, username=form.username.data,
+                        email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        flash('Başarılı bir şekilde kayıt oldunuz', 'success')
+        flash('Registered is success!', 'success')
         return redirect(url_for('login'))
     else:
-        return render_template('auth/register.html', form = form)
+        return render_template('auth/register.html', form=form)
+
 
 @app.route('/logout')
 def logout():
-    pass
+    session.clear()
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     db.create_all()
